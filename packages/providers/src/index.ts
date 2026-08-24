657112afa0154481c6da9dd3572914e3c216866c
YansIlinta <3764777815@qq.com>
2026-08-24 21:34:55 +0800

    Export hosted OpenAI ASR provider

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -2,5 +2,6 @@ export * from "./contracts.js";
 export * from "./fakes.js";
 export * from "./structured-output.js";
 export * from "./openai.js";
+export * from "./openai-asr.js";
 export * from "./openai-voice.js";
 export * from "./registry.js";
