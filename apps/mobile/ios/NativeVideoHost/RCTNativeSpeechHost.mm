4c9548593a07d81359b4836c07b1e7a03d049cbb
YansIlinta <3764777815@qq.com>
2026-08-24 21:57:21 +0800

    Bridge iOS NativeSpeechHost callbacks

---
+++ apps/mobile/ios/NativeVideoHost/RCTNativeSpeechHost.mm
@@ -0,0 +1,22 @@
+#import "RCTNativeSpeechHost.h"
+#import "VideoAgentMobile-Swift.h"
+
+@implementation RCTNativeSpeechHost {
+  NativeSpeechHostService *_service;
+}
+
+- (instancetype)init { if ((self = [super init])) { _service = [NativeSpeechHostService new]; } return self; }
++ (NSString *)moduleName { return @"NativeSpeechHost"; }
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params { return std::make_shared<facebook::react::NativeSpeechHostSpecJSI>(params); }
+
+- (void)transcribeOpenAI:(NSString *)requestJson resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [_service transcribeOpenAI:requestJson completion:^(NSString *value, NSError *error) {
+    error ? reject(@"SPEECH_PROVIDER", error.localizedDescription, error) : resolve(value);
+  }];
+}
+
+- (void)cancelTranscription:(NSString *)requestId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  [_service cancelTranscription:requestId];
+  resolve(nil);
+}
+@end
