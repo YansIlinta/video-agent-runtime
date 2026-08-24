26530b47f7ec6529215fe7c0fe9a409a2255cde1
YansIlinta <3764777815@qq.com>
2026-08-24 22:57:01 +0800

    Export Qwen3 voice provider

--- packages/speech/src/index.ts
+++ packages/speech/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./asr.js";
 export * from "./qwen3-asr.js";
 export * from "./tts.js";
+export * from "./qwen3-tts.js";
 export * from "./fusion.js";
 export * from "./whisperx.js";
 export * from "./voice.js";
