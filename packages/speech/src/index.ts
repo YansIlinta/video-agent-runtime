ced3dd5e59677e0a42172182b5c7131646b9394a
YansIlinta <3764777815@qq.com>
2026-08-24 22:54:37 +0800

    Export speech catalog and Qwen3 ASR provider

--- packages/speech/src/index.ts
+++ packages/speech/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./asr.js";
+export * from "./qwen3-asr.js";
 export * from "./tts.js";
 export * from "./fusion.js";
 export * from "./whisperx.js";
 export * from "./voice.js";
+export * from "./catalog.js";
