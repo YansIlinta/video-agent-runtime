2e322c33c0b5d3b09f6f72c2208452d03bc8bf9a
YansIlinta <3764777815@qq.com>
2026-08-24 21:57:12 +0800

    Add iOS NativeSpeechHost TurboModule wrapper

---
+++ apps/mobile/ios/NativeVideoHost/RCTNativeSpeechHost.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import "NativeSpeechHostSpec.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@interface RCTNativeSpeechHost : NSObject <NativeSpeechHostSpec>
+@end
+NS_ASSUME_NONNULL_END
